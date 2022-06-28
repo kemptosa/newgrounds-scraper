7c36e874089765cea33c8fdc4ca670f0b1c23e89
kemptosa <99417853+kemptosa@users.noreply.github.com>
2022-06-28 18:54:27 -0400

    Scraper is functional

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # newgrounds-scraper
-Currently non-functional; Newgrounds updated their site at some point, they no longer store posts in the script tags. Should make parsing easier, as it removes the nightmare that is trying to isolate data out of the JS.
+Gets a user's posts from newgrounds.com.
+There are two methods exposed for use, `init()`, and `get_posts()`
+
+```python
+init(dir: str) -> None:
+  # by default, the cache is created in the working directory, but you can specify a subdirectory to place the cache
+  # if you are not using the cache or are fine with it in the working directory, you need not call this function
+```
+```python
+get_posts(user: str, enable_cache: bool, max_posts: int):
+  # user is the username of the user to get posts for
+  # enable_cache should be enabled during testing; it saves a copy of the html response to reduce network load
+  # max_posts determines the maximum posts to return, default is 5
+```
--- scraper.py
+++ scraper.py
@@ -1,87 +1,95 @@
-from typing import Literal
+import json
 from bs4 import BeautifulSoup
-#from PIL import Image
 import requests
-#import imagehash
 
-CACHE_FILE = "cache.html"
+TEST_CACHE_FILE = "cache.html"
+WORKING_CACHE_FILE = "cache.json"
 DEFAULT_MAX_POSTS = 5
 
-class Post:
+cached_posts = {}
+
+def init(dir):
+  global TEST_CACHE_FILE
+  global WORKING_CACHE_FILE
+  global cached_posts
+  TEST_CACHE_FILE = dir + TEST_CACHE_FILE
+  WORKING_CACHE_FILE = dir + WORKING_CACHE_FILE
+  try:
+    with open(WORKING_CACHE_FILE, "r") as f:
+      print("Working Cache Found at " + WORKING_CACHE_FILE)
+      cached_posts = json.loads(f.read())
+  except FileNotFoundError:
+    print("No working cache file, creating one now at " + WORKING_CACHE_FILE)
+    with open(WORKING_CACHE_FILE, "x") as f:
+      f.write(json.dumps(cached_posts))
+
+def _write_wc_data():
+  with open(WORKING_CACHE_FILE, "w") as f:
+    f.write(json.dumps(cached_posts))
+
+class _Post:
   def __init__(self, post_link: str, image_url:str, post_title:str):
     self.post_link = post_link
     self.image_url = image_url
     self.post_title = post_title
+  def __str__(self) -> str:
+    return self.post_title + '\n  ' + self.image_url + '\n  ' + self.post_link
 
-def get_posts(user: str, enable_cache: bool, max_posts: int = DEFAULT_MAX_POSTS, check_extensions: list[Literal['.png', '.jpg', '.jpeg']] = ['.png']) -> list[Post]:
+def _create_cache_data(content):
+  try:
+    with open(TEST_CACHE_FILE, 'xb') as f:
+      f.write(bytes(content, 'UTF-8'))
+  except FileExistsError:
+    print("Cache File already exists")
+
+def _get_cache_data(url):
+  try:
+    with open(TEST_CACHE_FILE, 'rb') as f:
+      print("Cache Found, reading from cache")
+      return(str(f.read(), 'UTF-8'))
+  except FileNotFoundError:
+    print("No Cache, grabbing from web")
+    url_content = str(requests.get(url).content, 'UTF-8')
+    _create_cache_data(url_content)
+    return url_content
+
+def _get_url_data(url, enable_cache):
+  if enable_cache:
+    return _get_cache_data(url)
+  else:
+    return str(requests.get(url).content, 'UTF-8')
+
+def _get_all_links(soup: BeautifulSoup):
+  return soup.find_all("a")
+
+def _process_post_link(a):
+  title = str(a.h4.string)
+  url = a['href']
+  img_url = ''
+  if cached_posts.get(url):
+    print("getting " + title + " from disk")
+    img_url = cached_posts[url]['image_url']
+  else:
+    print("getting " + title + " from web")
+    url_content = str(requests.get(url).content, 'UTF-8')
+    soup = BeautifulSoup(url_content, "html.parser")
+    pod = soup.find('div', 'pod-body')
+    img_url = pod.find('img')['src']
+  new_post = _Post(url, img_url, title).__dict__
+  cached_posts[url] = new_post
+  return new_post
+
+def get_posts(user: str, enable_cache: bool, max_posts: int = DEFAULT_MAX_POSTS) -> list[_Post]:
  # newgrounds has no easy way to get the extension of a file
  # currently assumes png, implement check_extensions later
  # or find a better way of grabbing extension
 
  #enable_cache = True
  url = "https://" + user + ".newgrounds.com/art/"
- url_content = ""
-
- if enable_cache:
-  try:
-   with open(CACHE_FILE, "rb") as f:
-    print("Cache Found, reading from cache")
-    url_content = str(f.read(), 'UTF-8')
-  except FileNotFoundError:
-   print("No Cache, grabbing from web")
-   url_content = str(requests.get(url).content, 'UTF-8')
-   with open(CACHE_FILE, "xb") as f:
-    f.write(bytes(url_content, 'UTF-8'))
- else:
-  print("Cache Disabled, we do it live")
-  url_content = str(requests.get(url).content, 'UTF-8')
- #
+ url_content = _get_url_data(url, enable_cache)
 
  soup = BeautifulSoup(url_content, "html.parser")
- foundarray = ""
-
- for child in soup.find_all("a"):
-  str(child)
- #
-
- arrsplit = foundarray.splitlines()
- numer = 0
- newsplit = []
-
- for line in arrsplit:
-  if numer > 0:
-   newsplit.append(line.strip()[1:-2])
-  numer = numer + 1
- #
-
- newsplit.pop()
- newsplit[-1] = newsplit[-1] + ">"
-
- soup_split = []
-
- for line in newsplit:
-   soup_split.append(BeautifulSoup(line, "html.parser"))
- #
-
- snumer = 0
- ret_list = []
- for small_soup in soup_split:
-  if snumer == 0:
-   sshref = small_soup.a["href"]
-   thumbref = small_soup.a.div.img["src"]
-   post_title = small_soup.a.div.img["alt"]
-   split_ref = sshref.split("/")
-   art_name = split_ref[-1]
-   artist_name = split_ref[-2]
-   image_url = thumbref.replace("thumbnails", "images").replace(".png", "_" + artist_name + "_" + art_name + ".png").split("?")[0]
-   #im = Image.open(requests.get(image_url, stream=True).raw)
-   #img_hash = imagehash.whash(im)
-   
-   new_post = Post(sshref, image_url, post_title)
-   ret_list.append(new_post)
-  #snumer += 1
- return(ret_list[:max_posts])
-#
-def a_sum():
-  return 2
-#
+ links = [x for x in _get_all_links(soup) if x.h4]
+ posts = [_process_post_link(x) for x in links[:max_posts]]
+ _write_wc_data()
+ return posts
